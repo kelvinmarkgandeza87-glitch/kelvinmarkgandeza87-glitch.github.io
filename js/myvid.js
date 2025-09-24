bdef1fec9fa71fb23ea984a50bf2dedf4b8958dc
kelvinmarkgandeza87-glitch <kelvinmarkgandeza87@gmail.com>
2025-09-24 18:45:51 +0800

    Add files via upload

---
+++ js/myvid.js
@@ -0,0 +1,14 @@
+  <script>
+    function myFunction() {
+      let video = document.getElementById("urvid");
+      let btn = document.getElementById("pause_button");
+
+      if (video.paused) {
+        video.play();
+        btn.innerText = "Pause";
+      } else {
+        video.pause();
+        btn.innerText = "Play";
+        }
+    }
+  </script>
